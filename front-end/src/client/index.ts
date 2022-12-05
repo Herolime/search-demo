910fe806680cd692a2fb9725910494d546f62980
Herandy Santos <emilesanen@gmail.com>
2022-12-05 01:34:49 -0400

    Refactoring client file.

--- front-end/src/client/index.ts
+++ front-end/src/client/index.ts
@@ -1,21 +1,22 @@
-export interface title {
-    title: string
-    photo: string
-    description: string
-    shortDescription: string
+export interface Title {
+  title: string
+  photo: string
+  description: string
+  shortDescription: string
 }
 
 const API_URL = process.env.REACT_APP_API_URL ?? ''
 
 export const apiClient = {
-    search: async (searchQuery: string) => {
-        const search = await fetch(`${API_URL}/search?searchQuery=${searchQuery}`)
-        if (search.ok) {
-            const searchResponse = await search.json()
-            return searchResponse as title[]
-        }
-        else {
-            // do error handling here
-        }
+  search: async (searchQuery: string) => {
+    const search = await fetch(`${API_URL}/search?searchQuery=${searchQuery}`)
+    if (search.ok) {
+      const searchResponse = await search.json()
+      return searchResponse as Title[]
+    } else {
+      // do error handling here
+      console.log(search.status)
+      console.log(await search.json())
     }
+  }
 }
