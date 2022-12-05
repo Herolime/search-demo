98f62daa94860de2da70d61798296274f2623c72
Herandy Santos <emilesanen@gmail.com>
2022-12-05 01:37:13 -0400

    Adding routing to frontend

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react'
+import {
+  Routes,
+  Route
+} from 'react-router-dom'
+import './App.css'
+import { Record } from './pageComponents/Record'
+import { Search } from './pageComponents/Search'
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/record" element={<Record/>}>
+        </Route>
+        <Route path="/" element={<Search/>}>
+        </Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
