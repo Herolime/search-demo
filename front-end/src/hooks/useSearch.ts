66a7aae44d5285df140074f946b7e30fce481e25
Herandy Santos <emilesanen@gmail.com>
2022-12-05 01:35:31 -0400

    Refactoring hook due to lint rules.

--- front-end/src/hooks/useSearch.ts
+++ front-end/src/hooks/useSearch.ts
@@ -1,20 +1,23 @@
-import { useState } from "react"
-import { apiClient, title } from "../client"
+import { useState } from 'react'
+import { apiClient, Title } from '../client'
 
-export const useSearch = () => {
-    const [searchedTitles, setSearchedTitles] = useState<title[]>([])
+export const useSearch: () => {
+  searchedTitles: Title[]
+  searchTitles: (arg: string) => unknown
+} = () => {
+  const [searchedTitles, setSearchedTitles] = useState<Title[]>([])
 
-    const searchTitles = async (searchQuery: string) => {
-        if (searchQuery) {
-            const results = await apiClient.search(searchQuery)
-            if (results) {
-                setSearchedTitles(results) 
-            }
-        }
+  const searchTitles: (arg: string) => unknown = async (searchQuery: string) => {
+    if (searchQuery != null) {
+      const results = await apiClient.search(searchQuery)
+      if (results != null) {
+        setSearchedTitles(results)
+      }
     }
+  }
 
-    return {
-        searchedTitles,
-        searchTitles
-    }
+  return {
+    searchedTitles,
+    searchTitles
+  }
 }
